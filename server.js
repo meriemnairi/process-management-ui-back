04aef89a3c24676df143c32e8e8a0bbdc428e18c
Meriem Nairi <meriemn500@gmail.com>
2024-10-13 23:31:54 +0200

    changes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-
 mongoose
   .connect(
     "mongodb+srv://meriemnairi:Wd7rUbJ2tk7PphRV@cluster0.03yyn.mongodb.net/"
@@ -19,7 +17,6 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Route to add a new page
 app.post("/api/pages/add", async (req, res) => {
   const { title, icon, color, form, link } = req.body;
 
@@ -33,13 +30,11 @@ app.post("/api/pages/add", async (req, res) => {
   }
 });
 
-
 app.put("/api/pages/update/:id", async (req, res) => {
-  const { id } = req.params; 
-  const { title, icon, color, form, link } = req.body; 
+  const { id } = req.params;
+  const { title, icon, color, form, link } = req.body;
 
   try {
-    
     const updatedPage = await Page.findByIdAndUpdate(
       id,
       { title, icon, color, form, link },
@@ -50,13 +45,14 @@ app.put("/api/pages/update/:id", async (req, res) => {
       return res.status(404).json({ message: "Page not found" });
     }
 
-    res.status(200).json({ message: "Page updated successfully", page: updatedPage });
+    res
+      .status(200)
+      .json({ message: "Page updated successfully", page: updatedPage });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error updating page", error });
   }
 });
-
 
 app.get("/api/pages", async (req, res) => {
   try {
@@ -68,19 +64,14 @@ app.get("/api/pages", async (req, res) => {
   }
 });
 
-
-
 app.get("/api/pages/:id", async (req, res) => {
   try {
-    //console.log("Page ID:", req.params.id); 
-
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       console.log("Invalid page ID format");
       return res.status(400).send("Invalid page ID format");
     }
 
     const page = await Page.findById(req.params.id);
-    //console.log("Page found:", page); 
 
     if (!page) {
       console.log("Page not found for ID:", req.params.id);
@@ -91,13 +82,10 @@ app.get("/api/pages/:id", async (req, res) => {
     await page.save();
     res.json(page);
   } catch (err) {
-    console.error("Error fetching page:", err); 
+    console.error("Error fetching page:", err);
     res.status(500).send(err);
   }
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
